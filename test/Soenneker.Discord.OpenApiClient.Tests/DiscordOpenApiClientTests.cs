6fac7bcacc8d2b10a95b2f6fd83030c8f14c869b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:09 -0500

    TUnit conversion

--- test/Soenneker.Discord.OpenApiClient.Tests/DiscordOpenApiClientTests.cs
+++ test/Soenneker.Discord.OpenApiClient.Tests/DiscordOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Discord.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class DiscordOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class DiscordOpenApiClientTests : HostedUnitTest
 {
-    public DiscordOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public DiscordOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
