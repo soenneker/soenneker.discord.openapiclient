494bed24edd8a4c3c44b6b97f59c843abf0f073f
Jake Soenneker <jake@soenneker.com>
2026-08-01 17:58:03 +0000

    Automated update

--- src/Soenneker.Discord.OpenApiClient/Models/GuildRoleMemberCounts200ResponseSchema.cs
+++ src/Soenneker.Discord.OpenApiClient/Models/GuildRoleMemberCounts200ResponseSchema.cs
@@ -9,27 +9,27 @@ namespace Soenneker.Discord.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class GuildRoleMemberCounts200ResponseResponseJson : IAdditionalDataHolder, IParsable
+    public partial class GuildRoleMemberCounts200ResponseSchema : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Discord.OpenApiClient.Models.GuildRoleMemberCounts200ResponseResponseJson"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Discord.OpenApiClient.Models.GuildRoleMemberCounts200ResponseSchema"/> and sets the default values.
         /// </summary>
-        public GuildRoleMemberCounts200ResponseResponseJson()
+        public GuildRoleMemberCounts200ResponseSchema()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Discord.OpenApiClient.Models.GuildRoleMemberCounts200ResponseResponseJson"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Discord.OpenApiClient.Models.GuildRoleMemberCounts200ResponseSchema"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Discord.OpenApiClient.Models.GuildRoleMemberCounts200ResponseResponseJson CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Discord.OpenApiClient.Models.GuildRoleMemberCounts200ResponseSchema CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Discord.OpenApiClient.Models.GuildRoleMemberCounts200ResponseResponseJson();
+            return new global::Soenneker.Discord.OpenApiClient.Models.GuildRoleMemberCounts200ResponseSchema();
         }
         /// <summary>
         /// The deserialization information for the current model
